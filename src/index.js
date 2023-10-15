050dd956301bd88624053c810488e16218dad06e
Loïc Prosi <loicprosi@gmail.com>
2023-10-15 22:05:09 +0200

    feat: added router logic

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 
+import "./index.sass";
+
+import routesConfig from "./utils/routesConfig";
+import works from "./data/works.json";
+import skills from "./data/skills.json";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
+
+const routes = routesConfig(works, skills);
+const router = createBrowserRouter(routes);
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
