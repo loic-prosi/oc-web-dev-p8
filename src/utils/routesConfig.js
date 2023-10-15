271fba62b269836019a4b43ef1405efc9afbc308
Loïc Prosi <loicprosi@gmail.com>
2023-10-15 21:57:27 +0200

    feat: added react router config

---
+++ src/utils/routesConfig.js
@@ -0,0 +1,12 @@
+import Layout from "../pages/Layout";
+
+const routesConfig = (works, skills) => {
+  return [
+    {
+      path: "/",
+      element: <Layout />
+    }
+  ];
+};
+
+export default routesConfig;
