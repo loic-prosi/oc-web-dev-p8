9eeb88f169841f80f0df025a7f9a45db9b4dbbf4
Loïc Prosi <loicprosi@gmail.com>
2023-10-17 01:55:29 +0200

    feat: added error page

---
+++ src/pages/Error/Error.jsx
@@ -0,0 +1,32 @@
+import { useRouteError, Link } from "react-router-dom";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+
+const Error = () => {
+  const error = useRouteError();
+  return (
+    <div className="layout">
+      <div className="layout__container">
+        <Navbar />
+        <main className="error">
+          <div className="error__code-container">
+            <h2 className="error__code">{error.status}</h2>
+          </div>
+          <div className="error__message-container">
+            <p className="error__message">Oups ! Une erreur est survenue.</p>
+            <p className="error__infos">
+              Vous pouvez retourner sur la page d'accueil en cliquant sur le
+              bouton ci-dessous.
+            </p>
+            <Link className="error__home-button" to="/">
+              Retour sur la page d'accueil
+            </Link>
+          </div>
+        </main>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Error;
--- src/utils/routesConfig.js
+++ src/utils/routesConfig.js
@@ -1,11 +1,13 @@
 import Layout from "../pages/Layout";
 import Home from "../pages/Home";
+import Error from "../pages/Error";
 
 const routesConfig = (works, skills) => {
   return [
     {
       path: "/",
       element: <Layout />,
+      errorElement: <Error />,
       children: [
         {
           index: true,
