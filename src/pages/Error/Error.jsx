859883e1a9d8476b4f2df18d0cddced4e10a6440
Loïc Prosi <loicprosi@gmail.com>
2023-10-17 02:52:10 +0200

    feat: added ??? for unknown error code

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -4,13 +4,16 @@ import Footer from "../../components/Footer";
 
 const Error = () => {
   const error = useRouteError();
+  console.log("error: ", error);
   return (
     <div className="layout">
       <div className="layout__container">
         <Navbar />
         <main className="error">
           <div className="error__code-container">
-            <h2 className="error__code">{error.status}</h2>
+            <h2 className="error__code">
+              {error.status ? error.status : "???"}
+            </h2>
           </div>
           <div className="error__message-container">
             <p className="error__message">Oups ! Une erreur est survenue.</p>
