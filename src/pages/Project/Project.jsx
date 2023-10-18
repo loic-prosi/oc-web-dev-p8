505978bb1126da62e06a551b6d6bc54acff10fbe
Loïc Prosi <loicprosi@gmail.com>
2023-10-18 15:02:57 +0200

    feat: added work description in work data

--- src/pages/Project/Project.jsx
+++ src/pages/Project/Project.jsx
@@ -20,18 +20,7 @@ const Project = () => {
             <h2 className="project__title">{work.name}</h2>
           </header>
           <div className="project__skills">{renderSkills()}</div>
-          <p className="project__description">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque
-            omnis voluptatum velit. Delectus sapiente laboriosam rem laborum
-            repellat tenetur quo, incidunt cum. Fugiat veniam atque aperiam ad
-            corporis cumque et.
-          </p>
-          <p className="project__description">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga eius
-            illum tempora, harum praesentium a laboriosam quam possimus, dolore,
-            ducimus veniam aliquid dicta necessitatibus quae! Quae repellendus
-            blanditiis modi provident.
-          </p>
+          <p className="project__description">{work.description}</p>
         </article>
         <div className="project__image-container">
           <img className="project__image" src={work.image} alt={work.name} />
