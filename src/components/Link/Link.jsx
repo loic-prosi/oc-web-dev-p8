07fb4ecad9305e93c275bebcf2531eaef42ad5b6
Loïc Prosi <loicprosi@gmail.com>
2023-10-22 22:58:04 +0200

    fix: link is displayed when url is empty

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -8,20 +8,22 @@ const Link = ({ link }) => {
   const isLargeLink = size && size === "large";
   const linkClassName = `link ${isLargeLink ? "link--large" : ""}`;
 
-  if (download) {
+  if (url) {
+    if (download) {
+      return (
+        <a className={linkClassName} href={url} download>
+          {icon && <Icon name={icon} size={iconSize} />}
+          {name}
+        </a>
+      );
+    }
     return (
-      <a className={linkClassName} href={url} download>
+      <ReactRouterLink className={linkClassName} to={url}>
         {icon && <Icon name={icon} size={iconSize} />}
         {name}
-      </a>
+      </ReactRouterLink>
     );
   }
-  return (
-    <ReactRouterLink className={linkClassName} to={url}>
-      {icon && <Icon name={icon} size={iconSize} />}
-      {name}
-    </ReactRouterLink>
-  );
 };
 
 export default Link;
