1813cb77640d384ddd472505ac6fa835492da94a
Loïc Prosi <loicprosi@gmail.com>
2023-10-22 23:00:52 +0200

    feat: added portfolio github link in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,16 @@
+import Link from "../Link";
+
 const Footer = () => {
   return (
     <footer className="footer">
       <span>Loïc Prosi - 2023</span>
+      <Link
+        link={{
+          name: "Code source",
+          url: "https://github.com/loic-prosi/oc-web-dev-p8",
+          icon: "fa github"
+        }}
+      />
     </footer>
   );
 };
