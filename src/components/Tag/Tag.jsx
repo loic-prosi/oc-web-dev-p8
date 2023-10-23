7fae6c4a4eb0d2c010c43ef8c2862f282bef9b80
Loïc Prosi <loicprosi@gmail.com>
2023-10-23 07:35:14 +0200

    feat: added filters in projects page

--- src/components/Tag/Tag.jsx
+++ src/components/Tag/Tag.jsx
@@ -1,5 +1,15 @@
-const Tag = ({ type, text }) => {
-  const className = type && type === "card" ? "tag tag--card" : "tag";
+const Tag = ({ type, text, onClick, active }) => {
+  let className = type ? `tag tag--${type}` : "tag";
+  className = active ? `${className} tag--active` : className;
+
+  if (type === "filter") {
+    return (
+      <button className={className} value={text} onClick={onClick}>
+        {text}
+      </button>
+    );
+  }
+
   return <span className={className}>{text}</span>;
 };
 
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,19 +1,53 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 import Article from "../../components/Article";
 import Card from "../../components/Card";
+import Separator from "../../components/Separator";
+import Tag from "../../components/Tag/Tag";
 
 const Projects = () => {
   const { works } = useLoaderData();
+  const [filteredWorks, setFilteredWorks] = useState(works);
+  const [currentFilter, setCurrentFilter] = useState("Tous");
+
+  const filters = ["Tous", "Front-end", "Back-end", "Gestion de projet"];
+
+  const filterWorks = (e) => {
+    if (e.target.value !== "Tous") {
+      let filteredWorks = works.filter((work) =>
+        work.categories.includes(e.target.value)
+      );
+      setFilteredWorks(filteredWorks);
+    } else {
+      setFilteredWorks(works);
+    }
+    setCurrentFilter(e.target.value);
+  };
+
+  const renderFilters = () => {
+    return filters.map((filter, index) => (
+      <Tag
+        key={`${index}-${filter}`}
+        type="filter"
+        text={filter}
+        onClick={(e) => filterWorks(e)}
+        active={currentFilter === filter ? true : false}
+      />
+    ));
+  };
+
   return (
-    <main className="page">
+    <main className="page page--projects">
       <Article
         title="Mes projets"
         text="Découvrez la liste des projets ayant contribué à l'évolution de mes compétences."
       />
+      <Separator className="projects__separator" text="Filtres" />
+      <div className="projects__filters">{renderFilters()}</div>
       <div className="projects__list">
-        {works &&
-          works.map((work) => (
+        {filteredWorks &&
+          filteredWorks.map((work) => (
             <Card
               key={work.id}
               id={work.id}
